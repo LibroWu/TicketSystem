331b2d05f41f2864593c837cf1bfb3a5412053d5
LibroWu <731096310@qq.com>
2021-06-02 10:59:49 +0800

    change hash

--- src/includes/Algorithm.cpp
+++ src/includes/Algorithm.cpp
@@ -27,22 +27,16 @@ namespace LaMetropole {
         long long tmp = 0;
         //sub_hash1 197
         for (int i = 0; i < len; ++i) {
-            tmp = (tmp << 7) + (tmp << 6) + (tmp << 2) + tmp + input[i] * 3 + input[i] * input[i];
-            tmp ^= xor_num[abs(input[i]) % 4];
-            if (input[i] % 7 == 0) tmp <<= 1;
-            if (input[i] % 13 == 0) tmp >>= 1;
-            if (input[i] % 3 == 0) tmp ^= tmp >> 16;
+            tmp = (tmp << 7) + (tmp << 6) + (tmp << 2) + tmp + (input[i] << 2) + input[i] * input[i];
+            tmp ^= xor_num[abs(input[i]) & 1];
+            tmp ^= tmp>>16;
         }
         tmp <<= 32;
         long long tmp2 = 0;
         //sub_hash2 157
         for (int i = 0; i < len; ++i) {
-            tmp2 = (tmp2 << 7) + (tmp2 << 4) + (tmp2 << 3) + (tmp2 << 2) + tmp2 + input[i] * 11 +
-                   17 * input[i] * input[i];
-            tmp2 ^= xor_num[3 - abs(input[i]) % 4];
-            if (input[i] % 2 == 0) tmp2 <<= 1;
-            if (input[i] % 5 == 0) tmp2 >>= 1;
-            if (input[i] % 7 == 0) tmp2 ^= tmp2 >> 16;
+            tmp2 = (tmp2 << 7) + (tmp2 << 4) + (tmp2 << 3) + (tmp2 << 2) + tmp2 + input[i];
+            tmp2 ^= xor_num[3 - (abs(input[i]) & 1)];
             tmp2 ^= tmp2 << 16;
         }
         tmp ^= tmp2;
